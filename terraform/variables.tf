56a03dbd48940b985059059356e45c58e55a1ccf
Jayrivarex <urtedrox44@gmail.com>
2026-09-07 11:05:11 -0500

    fix:ajustar sku a f1 y actualizar nombres unicos de recursos

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,12 +10,12 @@ variable "location" {
 
 variable "acr_name" {
   type        = string
-  default     = "acrdevopslab2026"
+  default     = "acrdevopsjay2026" # Nombre único global
   description = "Nombre de Azure Container Registry"
 }
 
 variable "app_name" {
   type        = string
-  default     = "app-devops-lab-2026"
+  default     = "app-devops-jay-2026" # Nombre único global
   description = "Nombre de la Web App en Azure"
 }
